4598ad2c9db4c3d0dece39d64ad6a37de0e24e9e
Ricky Mormor <rickymormor1@gmail.com>
2022-01-18 13:32:42 +0000

    Add lifecycle

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -6,24 +6,9 @@ import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
-    ],
+    todos: [],
   };
+
   handleChange = (id) => {
     this.setState((prevState) => ({
       todos: prevState.todos.map((todo) => {
@@ -40,47 +25,62 @@ class TodoContainer extends React.Component {
 
   delTodo = (id) => {
     this.setState({
-      todos: [
-        ...this.state.todos.filter(todo => todo.id !== id),
-      ]
-    })
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
   };
 
-  addTodoItem = title => {
+  addTodoItem = (title) => {
     const newTodo = {
       id: uuidv4(),
       title: title,
       completed: false,
-  };
+    };
     this.setState({
-      todos: [...this.state.todos, newTodo]
-    })
+      todos: [...this.state.todos, newTodo],
+    });
   };
 
   setUpdate = (updatedTitle, id) => {
     this.setState({
-      todos: this.state.todos.map(todo => {
+      todos: this.state.todos.map((todo) => {
         if (todo.id === id) {
-          todo.title = updatedTitle
+          todo.title = updatedTitle;
         }
-        return todo
+        return todo;
       }),
-    })
+    });
+  };
+
+  componentDidMount() {
+    const temp = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem("todos", temp);
+    }
   }
 
   render() {
     return (
       <div className="container">
-      <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList
-          handleChangeProps={this.handleChange}
-          todos={this.state.todos}
-          deleteTodoProps={this.delTodo}
-          setUpdate={this.setUpdate}
-        />
-      </div>
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            handleChangeProps={this.handleChange}
+            todos={this.state.todos}
+            deleteTodoProps={this.delTodo}
+            setUpdate={this.setUpdate}
+          />
+        </div>
       </div>
     );
   }
